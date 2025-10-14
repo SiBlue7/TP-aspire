c7febfc9bb5273e50c9b3d34941909079d997c24
SiBlue <enzo20112002@gmail.com>
2025-10-14 11:57:16 +0200

    Step 3.1

--- projetMicrosoftTech.ApiService/obj/Debug/net9.0/projetMicrosoftTech.ApiService.AssemblyInfo.cs
+++ projetMicrosoftTech.ApiService/obj/Debug/net9.0/projetMicrosoftTech.ApiService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetMicrosoftTech.ApiService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903af81ac5b985d8f3ed383568555da09c283d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29455092fd3be938d4418540cb549d2ea8d9f029")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetMicrosoftTech.ApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetMicrosoftTech.ApiService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- projetMicrosoftTech.AppHost/AppHost.cs
+++ projetMicrosoftTech.AppHost/AppHost.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
+var sql = builder.AddSqlServer("sql")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume();
+var initScriptPath = Path.Join(Path.GetDirectoryName(typeof(Program).Assembly.Location), "init.sql");
+var db = sql.AddDatabase("KohakuDB")
+    .WithCreationScript(File.ReadAllText(initScriptPath));
 
-builder.AddProject<Projects.projetMicrosoftTech_ApiService>("apiservice");
+builder.AddProject<Projects.projetMicrosoftTech_ApiService>("apiservice")
+    .WithReference(sql)
+    .WaitFor(db);
 
 builder.Build().Run();
